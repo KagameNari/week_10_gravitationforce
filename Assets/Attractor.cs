f9c9569aed723dbe62eb1ad56ba0b07b845ce768
Kanyanat Leowphaiboon <kanyanat.leow@bumail.net>
2024-03-13 21:15:58 +0700

    attractor-planet

--- Assets/Attractor.cs
+++ Assets/Attractor.cs
@@ -4,12 +4,43 @@ using UnityEngine;
 
 public class Attractor : MonoBehaviour
 {
-    [SerializeField] public Rigidbody rb;
+    public Rigidbody rb; 
+    const float G = 6.674f;
+
+    static List<Attractor> attractors; 
+
+    private void OnEnable()
+    {
+        if (attractors == null)
+        {
+            attractors = new List<Attractor>();
+        }
+        attractors.Add(this);
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (Attractor attractor in attractors)
+        {
+            if (attractor != this)
+            {
+                Attract(attractor);
+            }
+        }
+    }
 
     void Attract(Attractor other)
     {
         Rigidbody rb2 = other.rb;
-    }
-    
 
+        Vector3 direction = rb.position - rb2.position; 
+
+        float distance = direction.magnitude;
+
+        float forceMagnitude = G * (rb.mass * rb2.mass) / Mathf.Pow(distance, 2);
+
+        Vector3 force = direction.normalized * forceMagnitude; 
+
+        rb2.AddForce(force);
+    }
 }
